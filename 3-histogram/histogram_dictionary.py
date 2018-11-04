7d4b7496b90eef83ae39d561e671295dd035f759
ruhsane <ruhsane924@gmail.com>
2018-11-04 12:22:17 -0800

    optimized histogram dict

---
+++ 3-histogram/histogram_dictionary.py
@@ -0,0 +1,22 @@
+def histogram():
+    histogram_dict = {}
+    with open('/Users/ruhsane/dev/courses/cs1.2/tweet-generator/3-histogram/source.txt','r') as f:
+        for line in f:
+            for word in line.split():
+                if word in histogram_dict:
+                    histogram_dict[word] += 1
+                else:
+                    histogram_dict[word]=1
+    return histogram_dict
+
+def unique_words(histogram):
+    unique = len(histogram.keys())
+    print("there are {} unique words".format(unique))
+
+def frequency(word, histogram):
+    print("{} appears {} times".format(word, histogram.get(word)))
+
+if __name__ == "__main__":
+    print(histogram())
+    unique_words(histogram())
+    # frequency("fish", histogram())
